9fb1878cf0a4b50ff874a69d96fa531012b36c85
Mohamed Harmanani <harma@Mohameds-MacBook-Pro.local>
2020-10-26 20:45:45 -0400

    landing page prelim draft

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,25 +1,31 @@
-import './Landing.css';
+import LandingNavbar from './LandingNavbar';
 
+import Lshape from '../assets/img/asset-3-L.svg';
+import ballshape from '../assets/img/asset-4-ball.svg';
+import './Landing.css';
 
 const LandingPage = () => {
     return (
-        <div className="main">
-          <header className="main-header">
-            <h1 className="main-title">
-              Connecting students virtually
-            </h1>
-            <a
-              className="get-started"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Get started
-            </a>
-            <div className='sub-title-container'>
-                <h3 className='sub-title-prompt'> Are you a professor? </h3>
-                <h3 className='sub-title-cta'> Add your class</h3>
+        <div>
+            <LandingNavbar />
+            <img src={Lshape} className='L-icon' width={256}/>
+            <img src={ballshape} className='ball-icon' width={256}/>
+            <div className="main">
+            <header className="main-header">
+                <h1 className="main-title"> Connecting students virtually </h1>
+                <a
+                className="get-started"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Get started
+                </a>
+                <div className='sub-title-container'>
+                    <h3 className='sub-title-prompt'> Are you a professor? </h3>
+                    <h3 className='sub-title-cta'> Add your class</h3>
+                </div>
+            </header>
             </div>
-          </header>
         </div>
     );
 }
