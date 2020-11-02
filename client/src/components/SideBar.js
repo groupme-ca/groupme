a228bf98912e2f1c56bd200cab6a1ab639e5fb58
deval-patel <devalrocket@gmail.com>
2020-11-02 13:20:42 -0500

    Formatting changes

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './SideBar.css';
-import logo from '../assets/img/logo.svg';
-import search from '../assets/img/search-icon.svg';
-import profile from '../assets/img/profile-icon.svg';
-import red_x from '../assets/img/red_x.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./SideBar.css";
+import logo from "../assets/img/logo.svg";
+import search from "../assets/img/search-icon.svg";
+import profile from "../assets/img/profile-icon.svg";
+import red_x from "../assets/img/red_x.png";
 
 class Sidebar extends React.Component {
     constructor(props) {
@@ -14,30 +14,37 @@ class Sidebar extends React.Component {
         };
     }
 
-    render () {
+    render() {
         return (
-            <div className='sidebar'>
-                <div className='sidebar-content'> 
-                    <Link to='/'> 
-                        <img id='logo' src={logo} width={64} /> 
-                    </Link>
-                    <br></br>
-                    <Link to='/profile'>
-                    <div class='sidebar-tab'> <img class='sidebar-img' src={profile} width={32} /> 
-                    <span class='sidebar-text'> My Profile </span> </div>
-                    </Link>
-                    <Link to='/welcome'>
-                    <div class='sidebar-tab'> <img class='sidebar-img' src={search} width={32} /> 
-                    <span class='sidebar-text'> Search </span> </div>
-                    </Link>
-                    <Link to='/'>
-                    <div class='sidebar-tab'> <img class='sidebar-img' src={red_x} width={32} /> 
-                    <span class='sidebar-text'> Sign Out </span> </div>
-                    </Link>
-                </div>
+            <div className="sidebar">
+                <Link to="/">
+                    <img id="logo" src={logo} width={64} />
+                </Link>
+                <br></br>
+                <Link to="/profile">
+                    <div class="sidebar-tab">
+                        {" "}
+                        <img class="sidebar-img" src={profile} width={32} />
+                        <span class="sidebar-text"> My Profile </span>{" "}
+                    </div>
+                </Link>
+                <Link to="/welcome">
+                    <div class="sidebar-tab">
+                        {" "}
+                        <img class="sidebar-img" src={search} width={32} />
+                        <span class="sidebar-text"> Search </span>{" "}
+                    </div>
+                </Link>
+                <Link to="/">
+                    <div class="sidebar-tab">
+                        {" "}
+                        <img class="sidebar-img" src={red_x} width={32} />
+                        <span class="sidebar-text"> Sign Out </span>{" "}
+                    </div>
+                </Link>
             </div>
-        )
+        );
     }
-  }
+}
 
 export default Sidebar;
