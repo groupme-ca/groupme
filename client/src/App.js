6e79fe23829ace818f48dfa8b25b7e1a15fc8f2b
mtthoag <moagazaly@yahoo.com>
2020-11-09 12:30:56 -0500

    added chat.js and chat.css

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,19 @@ import RecommendationPage from "./pages/Recommendations";
 import ProfilePage from "./pages/Profile";
 import Pusher from "pusher-js";
 
+<<<<<<< HEAD
 import { Provider } from "react-redux";
 import store from "./store";
 import { useEffect, Component } from "react";
+=======
+import LandingPage from './pages/Landing';
+import SignUpPage from './pages/SignUp';
+import SignInPage from './pages/SignIn';
+import RecommendationPage from './pages/Recommendations';
+import ProfilePage from './pages/Profile';
+import ChatPage from './pages/Chat';
+import Pusher from 'pusher-js';
+>>>>>>> 4b4798b (added chat.js and chat.css)
 
 class App extends Component {
 	//THIS SEGMENT IS FOR MAKING THE DB REAL TIME
@@ -27,6 +37,7 @@ class App extends Component {
 		store.dispatch(loadUser());
 	}
 
+<<<<<<< HEAD
 	render() {
 		return (
 			<Provider store={store}>
@@ -48,6 +59,32 @@ class App extends Component {
 			</Provider>
 		);
 	}
+=======
+  //THIS SEGMENT IS FOR MAKING THE DB REAL TIME
+  // useEffect(() => {
+  //   const pusher = new Pusher('d386d4bf8093a108cca2', {
+  //     cluster: 'us2'
+  //   });
+
+  //   const channel = pusher.subscribe('messages-channel');
+  //   channel.bind('inserted', function(data) {
+  //     alert(JSON.stringify(data));
+  //   });
+  // }, []);
+
+  return (
+    <Provider store={store}>
+    <Router>
+      <Route path='/' exact render={() => <LandingPage />} />
+      <Route path='/signup' exact render={() => <SignUpPage /> } />
+      <Route path='/signin' exact render={() => <SignInPage /> } />
+      <Route path='/welcome' exact render={() => <RecommendationPage /> } />
+      <Route path='/profile' exact render={() => <ProfilePage /> } />
+      <Route path='/chat' exact render={() => <ChatPage />} />
+    </Router>
+    </Provider>
+  );
+>>>>>>> 4b4798b (added chat.js and chat.css)
 }
 
 export default App;
