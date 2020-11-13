989507c7830fa6b990997f4d37053e13b16c53d0
Mohamed Harmanani <harma@Mohameds-MacBook-Pro.local>
2020-11-13 17:19:25 -0500

    Add default avatar for people with no profile pics

--- client/src/utils/UserCardUtils.js
+++ client/src/utils/UserCardUtils.js
@@ -6,11 +6,14 @@ import p1 from '../assets/img/person1.jpg';
 import p2 from '../assets/img/person2.jpg';
 import as from '../assets/img/george.jpg';
 
+import img_default from '../assets/img/default.png'
+
 const recommendations = {
     hobbies: [
         {name: "Vladimir Chadweeb", avatar: vlad, hobbies:['Anime', 'Gaming', 'Gamma Radiation & Chill', 'Anime Onesies', 'NOT people lol'], bio: "I wouldn't want to be my friend tbh"},
         {name: "Alick Professorson", avatar: alick, hobbies:['Anime', 'Professing'], bio: "Professor of Communications and Sociology at the University of Toronto"},
         {name: "Loretta B.", avatar: p1, hobbies:['Gaming', 'Reading'], bio: "Student Leader at someStudentAssocOrSmtg"},
+        {name: "Jordan P.", avatar: img_default, hobbies:['Reading'], bio: "I need to get a good profile picture!"},
         {name: "Mike Famli", avatar: p2, hobbies:['Gaming'], bio: "UofT CS 202? | Software Engineer @ GroupMe"},
     ],
     courses: [
