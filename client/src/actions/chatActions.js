4c5eed2ff5d008b48859bc6c450659996b53f093
deval-patel <devalrocket@gmail.com>
2020-11-14 20:36:03 -0500

    Get rid of non existant dispatch type

--- client/src/actions/chatActions.js
+++ client/src/actions/chatActions.js
@@ -1,14 +1,12 @@
-import axios from '../axios';
-import { GET_CHATS } from './types';
+import axios from "../axios";
+import { GET_CHATS } from "./types";
 
-export const getChats = (id) => dispatch => {
-    dispatch(setItemsLoading());
-    // This makes a GET request to our api route. 
-    axios
-        .get(`/api/messages/${id}`)
-        .then(res => 
-            dispatch({
-                type: GET_CHATS, 
-                payload: res.data
-            }));
+export const getChats = (id) => (dispatch) => {
+	// This makes a GET request to our api route.
+	axios.get(`/api/messages/${id}`).then((res) =>
+		dispatch({
+			type: GET_CHATS,
+			payload: res.data,
+		})
+	);
 };
