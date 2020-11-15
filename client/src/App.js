fc6a57afdf05dee2742d4fbbb3473f8cf69fcf2f
dsouzaly <lyle.dsouza@mail.utoronto.ca>
2020-11-14 20:37:12 -0500

    fixing merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,53 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { loadUser } from "./actions/authActions";
+import LandingPage from "./pages/Landing";
+import SignUpPage from "./pages/SignUp";
+import SignInPage from "./pages/SignIn";
+import RecommendationPage from "./pages/Recommendations";
+import ProfilePage from "./pages/Profile";
+import Pusher from "pusher-js";
 
-import LandingPage from './pages/Landing';
-import SignUpPage from './pages/SignUp';
-import SignInPage from './pages/SignIn';
-import RecommendationPage from './pages/Recommendations';
-import ProfilePage from './pages/Profile';
-import Pusher from 'pusher-js';
-import Messaging from './pages/Messaging.js'
+import { Provider } from "react-redux";
+import store from "./store";
+import { useEffect, Component } from "react";
 
-import { Provider } from 'react-redux';
-import store from './store';
-import { useEffect } from "react";
+class App extends Component {
+	//THIS SEGMENT IS FOR MAKING THE DB REAL TIME
+	// useEffect(() => {
+	//   const pusher = new Pusher('d386d4bf8093a108cca2', {
+	//     cluster: 'us2'
+	//   });
 
-function App() {
+	//   const channel = pusher.subscribe('messages-channel');
+	//   channel.bind('inserted', function(data) {
+	//     alert(JSON.stringify(data));
+	//   });
+	// }, []);
+	componentDidMount() {
+		store.dispatch(loadUser());
+	}
 
-  //THIS SEGMENT IS FOR MAKING THE DB REAL TIME
-  // useEffect(() => {
-  //   const pusher = new Pusher('d386d4bf8093a108cca2', {
-  //     cluster: 'us2'
-  //   });
-
-  //   const channel = pusher.subscribe('messages-channel');
-  //   channel.bind('inserted', function(data) {
-  //     alert(JSON.stringify(data));
-  //   });
-  // }, []);
-
-  return (
-    <Provider store={store}>
-    <Router>
-      <Route path='/' exact render={() => <LandingPage />} />
-      <Route path='/signup' exact render={() => <SignUpPage /> } />
-      <Route path='/signin' exact render={() => <SignInPage /> } />
-      <Route path='/welcome' exact render={() => <RecommendationPage /> } />
-      <Route path='/profile' exact render={() => <ProfilePage /> } />
-      <Route path='/messaging' exact render={() => <Messaging /> } />
-    </Router>
-    </Provider>
-  );
+	render() {
+		return (
+			<Provider store={store}>
+				<Router>
+					<Route path="/" exact render={() => <LandingPage />} />
+					<Route path="/signup" exact render={() => <SignUpPage />} />
+					<Route path="/signin" exact render={() => <SignInPage />} />
+					<Route
+						path="/welcome"
+						exact
+						render={() => <RecommendationPage />}
+					/>
+					<Route
+						path="/profile"
+						exact
+						render={() => <ProfilePage />}
+					/>
+				</Router>
+			</Provider>
+		);
+	}
 }
 
 export default App;
