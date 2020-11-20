0bc36676fa12c80a06fd46530bfb6e4df0ba0ae2
Mohamed Harmanani <harma@Mohameds-MacBook-Pro.local>
2020-11-20 16:48:50 -0500

    Profile modal basic design + functions

--- client/src/utils/UserCardUtils.js
+++ client/src/utils/UserCardUtils.js
@@ -10,15 +10,69 @@ import img_default from '../assets/img/default.png'
 
 const recommendations = {
     hobbies: [
-        {name: "Vladimir Chadweeb", avatar: vlad, hobbies:['Anime', 'Gaming', 'Gamma Radiation & Chill', 'Anime Onesies', 'NOT people lol'], bio: "I wouldn't want to be my friend tbh"},
-        {name: "Alick Professorson", avatar: alick, hobbies:['Anime', 'Professing'], bio: "Professor of Communications and Sociology at the University of Toronto"},
-        {name: "Loretta B.", avatar: p1, hobbies:['Gaming', 'Reading'], bio: "Student Leader at someStudentAssocOrSmtg"},
-        {name: "Jordan P.", avatar: img_default, hobbies:['Reading'], bio: "I need to get a good profile picture!"},
-        {name: "Mike Famli", avatar: p2, hobbies:['Gaming'], bio: "UofT CS 202? | Software Engineer @ GroupMe"},
+        {
+            name: "Vladimir Chadweeb", 
+            avatar: vlad, 
+            hobbies:['Anime', 'Gaming', 'Gamma Radiation & Chill', 'Anime Onesies', 'NOT people lol'], 
+            bio: "I wouldn't want to be my friend tbh",
+            courses: ["CSC490", "CSC420", "CSC369", "CCT110"],
+        },
+
+        {
+            name: "Alick Professorson", 
+            avatar: alick, 
+            hobbies:['Anime', 'Professing'], 
+            bio: "Professor of Communications and Sociology at the University of Toronto",
+            courses: ["SOC100", "COM202", "SOC480"],
+        },
+
+        {
+            name: "Loretta B.", 
+            avatar: p1, 
+            hobbies:['Gaming', 'Reading'], 
+            courses: ["POL113", "POL114", "CCT110"],
+            bio: "Student Leader at someStudentAssocOrSmtg",
+        },
+
+        {
+            name: "Jordan P.", 
+            avatar: img_default, 
+            hobbies:['Reading'], 
+            courses: ["PSY100", "PSY398"],
+            bio: "I need to get a good profile picture!"
+        },
+
+        {
+            name: "Mike Famli", 
+            avatar: p2, 
+            hobbies:['Gaming'], 
+            courses: ["CSC490", "CSC398", "CSC369"],
+            bio: "UofT CS 202? | Software Engineer @ GroupMe"
+        },
+
+        {
+            name: "Generic Mahnn", 
+            avatar: as, 
+            hobbies:['Gaming'], 
+            courses: ["CSC108", "CSC148", "MAT102"],
+            bio: "I am a generic man"
+        },
     ],
+
     courses: [
-        {name: "Lara Ken", avatar: lara, courses:['CSC490', 'CCT110'], bio: "Graphic design is my passion"},
-        {name: "George Kensington", avatar: as, courses:['CSC490', 'ART123'], bio: "Art and design is my entire personality"},
+        {
+            name: "Lara Ken", 
+            avatar: lara, 
+            courses:['CSC490', 'CCT110'], 
+            bio: "Graphic design is my passion"
+        },
+
+        {
+            name: "George Kensington", 
+            avatar: as, 
+            courses:['CSC490', 'ART123'], 
+            bio: "Art and design is my entire personality"
+        },
     ]
 };
 
