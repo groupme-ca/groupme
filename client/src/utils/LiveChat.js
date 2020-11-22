7b96efdcc908a7b638c157b03bf458c58180c8c4
mtthoag <moagazaly@yahoo.com>
2020-11-22 15:22:40 -0500

    added logic to display groups and dms seperatly, fixed a bug where a user was connecting to pusher multiple times whenever the page refreshed, removed old chat.js file, changed the server side channel notification which was hard coded

--- client/src/utils/LiveChat.js
+++ client/src/utils/LiveChat.js
@@ -7,38 +7,49 @@ import { useState, useEffect } from "react"
 
 const LiveChat = (state) => {
     // TODO: figure out how to unscubscribe later
+        const [channels, setChannels] = useState([]);
+        const [pusher, setPusher] = useState({});
 
         useEffect(() => {
-            const pusher = new Pusher('d386d4bf8093a108cca2', {
-                cluster: 'us2'
-            });
+            setPusher(new Pusher('d386d4bf8093a108cca2', {
+                cluster: 'us2'}));
+            
+        }, []);
+        
+        useEffect(() => {    
+            var channel;
+            const newChannel = []
             if (state.auth.token !== null){
                 state.auth.user.ChatIds.forEach(id => {
-                    const channel = pusher.subscribe(id);
+                    channel = pusher.subscribe(id);
+                    newChannel.push(channel);
                     channel.bind('updated', (data) => {
                         state.getChats(data);
                     });
+                });
+                // console.log(newChannel, 'newchannel');
+                setChannels(newChannel);
+    
+                // console.log('channels', channels, channels.length);      
 
-                    return() => {
-                        channel.unbind_all();
-                        channel.unsubscribe();
-                    };                
-                });    
             };
+            // setChannels(["hello"]);
+            // console.log(channels, 'final');
+            // return() => {
+            //     channels.forEach(channel => {
+            //         channel.unbind_all();
+            //         channel.unsubscribe();            
+            //     });
+                // console.log(channels);
+                // console.log("-");
+                // console.log(channels);
+
+            //};
         }, [state.chats]); 
     
     return (null);
-}
-
-    /* 
-    currently when a message is sent, user is allerted and a new chat is created. need to add new behaviour in the message backend
-    so when a message is sent to a specific chat id, it is appended to the end (maybe find and update with a new chat that 
-    includes the latest message). Then all the user has to do is request that specific chat id again so need to make a redux action that 
-    only gets 1 id (or just add the for loop in sign in instead of in the action).    
-    Since i switched to update rather than insert, i have to listen to the chat ids channel instead of listening to my own id
-    */
+};
         
-
 // This is the current state in the store.
 const mapStateToProps = (state) => ({
 	auth: state.auth,
