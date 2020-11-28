dbcd8a4abc113005f378039b02fd4e1c9ee7e914
deval-patel <devalrocket@gmail.com>
2020-11-27 19:06:36 -0500

    get rid of unnecessary axios file

--- client/src/actions/chatActions.js
+++ client/src/actions/chatActions.js
@@ -9,10 +9,35 @@ import { GET_CHATS, START_SWITCH, END_SWITCH } from "./types";
 export const getChats = (id) => (dispatch) => {
 	// dispatch(getChatStart());
 	// This makes a GET request to our api route
+<<<<<<< HEAD
 	axios.get(`/api/messages/get_chat/${id}`).then((res) => {
 		dispatch({
 			type: GET_CHATS,
 			payload: res.data,
 		});
 	});
+=======
+	axios.get(`/api/messages/${id}`).then((res) =>
+		dispatch({
+			type: GET_CHATS,
+			payload: res.data,
+		})
+	);
+};
+
+export const sendMessage = (chat) => (dispatch) => {
+	// dispatch(getChatStart());
+	// This makes a GET request to our api route
+	axios.put(`/api/messages/${chat._id}`, chat).then((res) =>
+		dispatch({
+			type: GET_CHATS,
+			payload: res.data,
+		})
+	);
+
+	// (res) = {
+	// console.log("1");
+	// dispatch(getChatSuccess(res.data));
+	//      }
+>>>>>>> get rid of unnecessary axios file
 };
