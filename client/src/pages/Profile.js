a1bf89fc8c9cdd45a467722ddd05d89023981c33
Mohamed Harmanani <38477796+mharmanani@users.noreply.github.com>
2020-11-27 20:06:48 -0500

    Update Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -21,11 +21,6 @@ class ProfilePage extends React.Component {
 	}
 	componentDidMount() {
 		this.setState({ user: this.props.auth.user });
-		console.log(
-			this.props.auth.user,
-			this.props.auth,
-			this.state
-		)
 	}
 
 	formEvent = ({ target }) => {
