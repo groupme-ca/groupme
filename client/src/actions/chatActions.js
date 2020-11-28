aaa42fcbadec5afa78c267fb5c380fc125e631c8
deval-patel <devalrocket@gmail.com>
2020-11-27 19:06:36 -0500

    get rid of unnecessary axios file

--- client/src/actions/chatActions.js
+++ client/src/actions/chatActions.js
@@ -1,12 +1,10 @@
-import axios from '../axios';
-import { GET_CHATS, CHAT_END_SWITCH, CHAT_START_SWITCH} from './types';
-
+import axios from "../axios";
+import { GET_CHATS, CHAT_END_SWITCH, CHAT_START_SWITCH } from "./types";
 
 // const getChatSuccess = (chats) => ({
 // 	type: GET_CHATS,
 // 	payload: chats,
 // });
-
 
 export const chatStartSwitch = () => {
 	return {
@@ -19,53 +17,41 @@ export const chatEndSwitch = () => {
 	};
 };
 export const getChats = (id) => async (dispatch) => {
-    // dispatch(getChatStart());
-    // This makes a GET request to our api route
-    await axios
-    .get(`/api/messages/get_chat/${id}`)
-    .then((res) => {
-        dispatch({
-            type: GET_CHATS,
-            payload: res.data, 
-        });
-    }      
-    );      
+	// dispatch(getChatStart());
+	// This makes a GET request to our api route
+	await axios.get(`/api/messages/get_chat/${id}`).then((res) => {
+		dispatch({
+			type: GET_CHATS,
+			payload: res.data,
+		});
+	});
 };
-
 
 export const createChat = (chat, participants) => async (dispatch) => {
-    // dispatch(getChatStart());
-    // This makes a GET request to our api route
-    await axios
-    .post(`/api/messages/new_chat`, chat)
-    .then((res) => {
-        dispatch({
-            type: GET_CHATS,
-            payload: res.data, 
-        });
-        const l = participants.length;
-        var cids = [];
-        var i =0;
-        for(i =0; i < l; i++){
-            cids = participants[i].ChatIds.concat([res.data._id]);
-            // if(i === l-1){
-            //     axios.put(`/api/users/${participants[i].id}`, {ChatIds: cids}).then(
-            //         axios.get("/api/users").then((res3) =>
+	// dispatch(getChatStart());
+	// This makes a GET request to our api route
+	await axios.post(`/api/messages/new_chat`, chat).then((res) => {
+		dispatch({
+			type: GET_CHATS,
+			payload: res.data,
+		});
+		const l = participants.length;
+		var cids = [];
+		var i = 0;
+		for (i = 0; i < l; i++) {
+			cids = participants[i].ChatIds.concat([res.data._id]);
+			// if(i === l-1){
+			//     axios.put(`/api/users/${participants[i].id}`, {ChatIds: cids}).then(
+			//         axios.get("/api/users").then((res3) =>
 
-            //             dispatch({
-            //                 type: GET_USERS,
-            //                 payload: res3.data,
-            //             }))
-            //         );                        
-            // }else{
-            axios.put(`/api/users/${participants[i].id}`, {ChatIds: cids});                        
-            // };
-        
-            
-        }
-    }      
-    );      
+			//             dispatch({
+			//                 type: GET_USERS,
+			//                 payload: res3.data,
+			//             }))
+			//         );
+			// }else{
+			axios.put(`/api/users/${participants[i].id}`, { ChatIds: cids });
+			// };
+		}
+	});
 };
-    
-
-
