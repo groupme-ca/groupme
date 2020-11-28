c992b439403af0a1c8d30a5ba038810b8138a5ed
deval-patel <devalrocket@gmail.com>
2020-11-27 20:11:16 -0500

    start working on addfriend

--- client/src/pages/Recommendations.js
+++ client/src/pages/Recommendations.js
@@ -428,7 +428,7 @@ class RecommendationPage extends React.Component {
 		this.foo();
 	}
 
-	async foo() {
+	async addFriend() {
 		await this.props.getUsers();
 		const users = this.props.user.users;
 		console.log(users);
