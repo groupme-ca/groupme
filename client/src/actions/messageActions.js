6631a0c8284d44357eab5aefdcf2472e656786b0
deval-patel <devalrocket@gmail.com>
2020-11-30 16:24:52 -0500

    remove axios file

--- client/src/actions/messageActions.js
+++ client/src/actions/messageActions.js
@@ -1,12 +1,11 @@
-import axios from '../axios';
-import {NEW_MESSAGE, SEND_MESSAGE, START_SWITCH, END_SWITCH  } from './types';
-
+import axios from "axios";
+import { NEW_MESSAGE, SEND_MESSAGE, START_SWITCH, END_SWITCH } from "./types";
 
 export const newMessage = (msg) => (dispatch) => {
-    dispatch({
-        type: NEW_MESSAGE,
-        payload: msg
-    });
+	dispatch({
+		type: NEW_MESSAGE,
+		payload: msg,
+	});
 };
 
 export const startSwitch = () => {
@@ -19,31 +18,26 @@ export const endSwitch = () => {
 		type: END_SWITCH,
 	};
 };
-    
+
 export const sendMessage = (msg) => (dispatch) => {
-    // dispatch(getChatStart());
-    // This makes a GET request to our api route
-    axios
-    .put(`/api/messages/new_msg`, msg)
-    .then((res) => 
-        dispatch({
-            type: SEND_MESSAGE,
-        })
-    );                  
+	// dispatch(getChatStart());
+	// This makes a GET request to our api route
+	axios.put(`/api/messages/new_msg`, msg).then((res) =>
+		dispatch({
+			type: SEND_MESSAGE,
+		})
+	);
 };
 
 export const getMessages = (id) => (dispatch) => {
-    // dispatch(getChatStart());
-    // This makes a GET request to our api route
-    axios
-    .get(`/api/messages/${id}`)
-    .then((res) => {
-        res.data.forEach(msg => {
-            dispatch(
-                {
-                    type: NEW_MESSAGE,
-                    payload: msg, 
-            });
-        });
-    });                 
+	// dispatch(getChatStart());
+	// This makes a GET request to our api route
+	axios.get(`/api/messages/${id}`).then((res) => {
+		res.data.forEach((msg) => {
+			dispatch({
+				type: NEW_MESSAGE,
+				payload: msg,
+			});
+		});
+	});
 };
