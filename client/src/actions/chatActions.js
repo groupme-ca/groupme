687a02f7e065f303e9f7d19660f57f711e4de193
deval-patel <devalrocket@gmail.com>
2020-12-05 07:23:31 -0500

    Fix merge mistake

--- client/src/actions/chatActions.js
+++ client/src/actions/chatActions.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { GET_CHATS, START_SWITCH, END_SWITCH } from "./types";
+import axios from "../axios";
+import { GET_CHATS } from "./types";
 
 // const getChatSuccess = (chats) => ({
 // 	type: GET_CHATS,
@@ -9,35 +9,10 @@ import { GET_CHATS, START_SWITCH, END_SWITCH } from "./types";
 export const getChats = (id) => (dispatch) => {
 	// dispatch(getChatStart());
 	// This makes a GET request to our api route
-<<<<<<< HEAD
 	axios.get(`/api/messages/get_chat/${id}`).then((res) => {
 		dispatch({
 			type: GET_CHATS,
 			payload: res.data,
 		});
 	});
-=======
-	axios.get(`/api/messages/${id}`).then((res) =>
-		dispatch({
-			type: GET_CHATS,
-			payload: res.data,
-		})
-	);
-};
-
-export const sendMessage = (chat) => (dispatch) => {
-	// dispatch(getChatStart());
-	// This makes a GET request to our api route
-	axios.put(`/api/messages/${chat._id}`, chat).then((res) =>
-		dispatch({
-			type: GET_CHATS,
-			payload: res.data,
-		})
-	);
-
-	// (res) = {
-	// console.log("1");
-	// dispatch(getChatSuccess(res.data));
-	//      }
->>>>>>> get rid of unnecessary axios file
 };
