5cc2a56990355b058e7becf75c00f7ca702528cb
deval-patel <devalrocket@gmail.com>
2020-12-07 00:11:18 -0500

    Add more constant hobbies

--- client/src/utils/SignUpOptions.js
+++ client/src/utils/SignUpOptions.js
@@ -1,16 +1,34 @@
 const options = {
-    hobbies: [
-        { value: 'anime', label: 'Anime' },
-        { value: 'gaming', label: 'Gaming' },
-        { value: 'gamma_rad', label: 'Gamma Radiation' }
-    ],
-    courses: [
-        {value: 'csc490', label: 'CSC490'},
-        {value: 'csc420', label: 'CSC420'},
-        {value: 'csc318', label: 'CSC318'},
-        {value: 'cct110', label: 'CCT110'},
-        {value: 'cct109', label: 'CCT109'},
-    ],
+	hobbies: [
+		{ value: "anime", label: "Anime" },
+		{ value: "gaming", label: "Gaming" },
+		{ value: "gamma_rad", label: "Gamma Radiation" },
+		{ value: "hiking", label: "Hiking" },
+		{ value: "camping", label: "Camping" },
+		{ value: "running", label: "Running" },
+		{ value: "Archery", label: "Archery" },
+		{ value: "Parkour", label: "Parkour" },
+		{ value: "Astronomy", label: "Astronomy" },
+		{ value: "Kite Flying", label: "Kite Flying" },
+		{ value: "Stamp Collecting", label: "Stamp Collecting" },
+		{ value: "Art Collecting", label: "Art Collecting" },
+		{ value: "Cooking", label: "Cooking" },
+		{ value: "Baking", label: "Baking" },
+		{ value: "Sewing", label: "Sewing" },
+		{ value: "camping", label: "Camping" },
+		{ value: "camping", label: "Camping" },
+		{ value: "camping", label: "Camping" },
+		{ value: "camping", label: "Camping" },
+		{ value: "camping", label: "Camping" },
+		{ value: "camping", label: "Camping" },
+	],
+	courses: [
+		{ value: "csc490", label: "CSC490" },
+		{ value: "csc420", label: "CSC420" },
+		{ value: "csc318", label: "CSC318" },
+		{ value: "cct110", label: "CCT110" },
+		{ value: "cct109", label: "CCT109" },
+	],
 };
 
 export default options;
